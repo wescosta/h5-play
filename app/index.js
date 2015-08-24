3dcf9de642d87cd03bda5ea9dc8a3d289c192ebd
Wesley Anemam <wesleylira@gmail.com>
2015-08-24 22:28:30 +0100

    File serializer add to save the videos list into a json file

--- app/index.js
+++ app/index.js
@@ -1,6 +1,14 @@
 'use strict'
 
-import * as async  from  'async'
-import * as videos from  'videos'
+import * as async  from 'async'
+import * as videos from 'videos'
+import * as file   from 'serializer'
 
-async.task(videos.lookup().then(files => console.log(files)), 'Looking for videos files')
+async.task(
+	videos.lookup().then(
+		list => file.serialize(`${__dirname}/node_modules/www/videos.json`, list).then(
+			msg => console.log(msg)
+		)
+	),
+ 'Looking for videos and preparing them to be loaded into the playlist'
+)
