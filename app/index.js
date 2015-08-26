89af96af5dd7c7c60ecfcdd3eb345c3c2ed85d4f
Wesley Anemam <wesleylira@gmail.com>
2015-08-26 04:30:29 +0100

    Adding feature to open the video playlist in the browser

--- app/index.js
+++ app/index.js
@@ -1,17 +1,28 @@
 'use strict'
 
-import * as async  from 'async'
-import * as videos from 'videos'
-import * as file   from 'serializer'
+import * as async   from 'async'
+import * as videos  from 'videos'
+import * as file    from 'serializer'
+import * as browser from 'browser'
+
+let folders = {
+	www: `${__dirname}/node_modules/www` 
+}
 
 async.task(
 	videos.lookup().then(
 		list => file.serialize(
-			`${__dirname}/node_modules/www/generated-list.js`, 
+			`${folders.www}/generated-list.js`, 
 			`window.videos = ${JSON.stringify(list)}`
-		).then(
-			msg => console.log(msg)
-		)
+		).then(() => {
+			let playlist = `file:///${folders.www}/index.html`
+
+			browser.open(playlist).then(
+				loaded => console.log('Yep. Your playlist has been loaded. Enjoy!')
+			).catch(
+				error  => console.trace('Ops! Something went wrong. I could not open', playlist, error)
+			)
+		})
 	),
  'Looking for videos and preparing them to be loaded into the playlist'
 )
